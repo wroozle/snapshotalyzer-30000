553c898b0a045505613afaffbd4cb12e26a41938
Terry Russell <wroozle@gmail.com>
2020-09-06 14:45:43 -0700

    Handle an error from AWS when stopping a pending instance or starting a stopping one

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -83,7 +84,6 @@ def create_snapshots(project):
     instances = filter_instances(project)
 
     for i in instances:
-
         print("Stopping {0}...".format(i.id))
 
         i.stop()
@@ -133,7 +133,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -147,7 +151,11 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
